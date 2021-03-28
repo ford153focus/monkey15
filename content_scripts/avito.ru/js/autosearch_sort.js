28708bf73032903404bef20e40655498b2544666
Joe Ford <ford153focus@gmail.com>
2021-03-28 13:29:31 +0300

    avt: enhance autosearch page

--- content_scripts/avito.ru/js/autosearch_sort.js
+++ content_scripts/avito.ru/js/autosearch_sort.js
@@ -10,6 +10,12 @@ items = document.querySelectorAll('[data-marker="favorite-search"]');
 
         return cat1===cat2 ? title1.localeCompare(title2) : cat1.localeCompare(cat2);
     })
+    .sort((item1, item2) => {
+        let newAdsIndicator1 = item1.querySelectorAll('div[class^="index-count"] div[class^="index-blue-round"]').length;
+        let newAdsIndicator2 = item2.querySelectorAll('div[class^="index-count"] div[class^="index-blue-round"]').length;
+
+        return newAdsIndicator2-newAdsIndicator1;
+    })
     .map((item) => {
         items[0].parentElement.appendChild(item);
     });
