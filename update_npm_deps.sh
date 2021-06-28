ccbfc919d5ddf30026abb00da8101b73c586f46f
Joe Ford <ford153focus@gmail.com>
2021-06-28 20:25:01 +0300

    clean up and update deps

--- update_npm_deps.sh
+++ update_npm_deps.sh
@@ -1,5 +1,2 @@
-#!/usr/bin/env bash
-for i in $(npm outdated --depth=0 | sed '1d' | awk '{print $1}'); do
-    echo "NPM :: $i"; 
-    npm install "$i" --save
-done
+npm update --save
+npm update --save-dev
